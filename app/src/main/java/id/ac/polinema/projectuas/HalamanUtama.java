d038b581da3955e9d74e3f020a542fd9e63a9107
febtrianiari <febtria.niari@gmail.com>
2019-11-26 21:06:07 +0700

    SplashScreen

---
+++ app/src/main/java/id/ac/polinema/projectuas/HalamanUtama.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.projectuas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HalamanUtama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_utama);
+    }
+}
