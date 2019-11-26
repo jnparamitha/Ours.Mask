24fa7900c485b1f11387db5f2cb583d691b82266
jnparamitha <jnparamitha@gmail.com>
2019-11-26 23:39:18 +0700

    Halaman Utama

--- app/src/main/java/id/ac/polinema/projectuas/HalamanUtama.java
+++ app/src/main/java/id/ac/polinema/projectuas/HalamanUtama.java
@@ -2,7 +2,9 @@ package id.ac.polinema.projectuas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HalamanUtama extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class HalamanUtama extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_utama);
+    }
+
+    public void handlerClickLogin(View view) {
+    }
+
+    public void handlerClickRegistrasi(View view) {
+        Intent intent = new Intent(this, HalamanRegister.class);
+        startActivity(intent);
     }
 }
